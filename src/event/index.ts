8e290a67461f13dbe120d6c7b30bfec09ccd76bb
JiPai <3956400+PaiJi@users.noreply.github.com>
2024-05-03 00:33:33 +0800

    feat: return multi events (#1)

--- src/event/index.ts
+++ src/event/index.ts
@@ -1,16 +1,22 @@
+import { Hono } from "hono";
+import Omit from "lodash/omit";
 import xataInstance from "@/database/helper";
 import imageURL from "@/utils/image";
-import { Hono } from "hono";
+
 const event = new Hono();
-import Omit from "lodash/omit";
 
 event.get("/recent", async (c) => {
   const xata = xataInstance(c);
+  const keepOld = c.req.query("keep_old") === "true";
+
   try {
-    const result = await xata.db.event
+    let events = await xata.db.event
       .filter({
         //过滤掉今天之前已经结束的，即目标展会的结束日期比今天大
-        endDate: { $ge: new Date() },
+        endDate: {
+          $ge: new Date(),
+          $le: new Date(new Date().setDate(new Date().getDate() + 30)),
+        },
       })
       .sort("startDate", "asc")
       .select([
@@ -28,30 +34,57 @@ event.get("/recent", async (c) => {
         "organization.slug",
         "organization.coverUrl",
       ])
-      .getFirst();
-    if (!result) throw new Error();
+      .getMany();
+    if (events.length === 0 && keepOld) {
+      const lastestEvent = await xata.db.event
+        .filter({
+          //找出比今天旧的展会
+          endDate: {
+            $le: new Date(),
+          },
+        })
+        .sort("startDate", "desc")
+        .select([
+          "name",
+          "scale",
+          "slug",
+          "startDate",
+          "endDate",
+          "status",
+          "coverUrl",
+          "detail",
+          "city",
+          "address",
+          "organization.name",
+          "organization.slug",
+          "organization.coverUrl",
+        ])
+        .getFirst();
+      lastestEvent && events.push(lastestEvent);
+    }
 
-    const finalResult = {
-      ...Omit(result, ["id", "xata", "organization.id", "organization.xata"]),
-      ...(result.coverUrl ? { coverUrl: imageURL(result.coverUrl) } : {}),
+    const enhancedEvents = events.map((event) => ({
+      ...Omit(event, ["id", "xata", "organization.id", "organization.xata"]),
+      ...(event.coverUrl ? { coverUrl: imageURL(event.coverUrl) } : {}),
       organization: {
-        ...result.organization,
-        ...(result.organization?.coverUrl
-          ? { coverUrl: imageURL(result.organization?.coverUrl) }
+        ...Omit(event.organization, ["id", "xata"]),
+        ...(event.organization?.coverUrl
+          ? { coverUrl: imageURL(event.organization?.coverUrl) }
           : {}),
-        globalUrl: `https://www.furryeventchina.com/${result?.organization?.slug}/`,
-        cnUrl: `https://www.furrycons.cn/${result?.organization?.slug}/`,
+        globalUrl: `https://www.furryeventchina.com/${event?.organization?.slug}/`,
+        cnUrl: `https://www.furrycons.cn/${event?.organization?.slug}/`,
       },
 
-      globalUrl: `https://www.furryeventchina.com/${result?.organization?.slug}/${result?.slug}`,
-      cnUrl: `https://www.furrycons.cn/${result?.organization?.slug}/${result?.slug}`,
-    };
+      globalUrl: `https://www.furryeventchina.com/${event?.organization?.slug}/${event?.slug}`,
+      cnUrl: `https://www.furrycons.cn/${event?.organization?.slug}/${event?.slug}`,
+    }));
+
     return c.json({
-      total: 1,
-      data: [finalResult],
+      total: events.length,
+      data: enhancedEvents,
     });
   } catch (error) {
-    return c.json({ info: "No event available", total: 0, data: [] }, 404);
+    return c.json({ info: "No event available", total: 0, data: [] }, 204);
   }
 });
 
